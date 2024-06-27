325fcf2834d0dbe3857099ec1057e428023eb747
sindhujaSirigeri <sindhujaSirigeri@users.noreply.github.com>
2024-06-27 22:12:23 +0800

    final

--- static/js/app.js
+++ static/js/app.js
@@ -7,22 +7,24 @@ function buildMetadata(sample){
 
     // Filter the metadata for the object with the desired sample number
     let result = metadata.filter(sampleObj => sampleObj.id == sample)[0];
-    console.log("Filtered Metadata:", result);
+    // console.log("Filtered Metadata:", result);
 
     // Use d3 to select the panel with id of `#sample-metadata`
-    let panel = d3.select("#metadata");
+    let panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-    panel.html("");
-
+    panel.html(""); 
+  
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-    Object.entries(result).forEach(([key, value]) => {
-    panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    for(key in result){
+      //console.log(key,filtered[value]);
+      panel.append("h6").text(`${key}: ${result[key]}`);
+    };
 
   });
-});
-}
+};
+
 
 // function to build both charts
 function buildCharts(sample) {
@@ -33,44 +35,46 @@ function buildCharts(sample) {
     let samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
-    let result = samples.filter(sampleObj => sampleObj.id == sample)[0];
+    let resultsArray = samples.filter(obj=> obj.id == sample);
+    let result = resultsArray[0]; 
 
     // Get the otu_ids, otu_labels, and sample_values
     let otu_ids = result.otu_ids;
     let otu_labels = result.otu_labels;
     let sample_values = result.sample_values;
-    console.log("OTU IDs:", otu_ids);
-    console.log("OTU Labels:", otu_labels);
-    console.log("Sample Values:", sample_values);
+    // console.log("OTU IDs:", otu_ids);
+    // console.log("OTU Labels:", otu_labels);
+    // console.log("Sample Values:", sample_values);
 
     // Build a Bubble Chart
-    var bubbleLayout = {
+    let bubbleLayout = {
       title: "Bacteria Cultures Per Sample",
       margin: { t: 0 },
       hovermode: "closest",
-      xaxis: { title: "OTU ID" },
+      xaxis: {title: "OTU ID" },
+      yaxis: {title: "Number of Bacteria"},
       margin: { t: 30}
   };
-  var bubbleData = [
-      {
-          x: otu_ids,
-          y: sample_values,
-          text: otu_labels,
-          mode: "markers",
-          marker: {
-              size: sample_values,
-              color: otu_ids,
-              colorscale: "Earth"
-          }
-      }
-  ];
+      let bubbleData = [
+        {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        }
+      ];
 
     // Render the Bubble Chart
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-  let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
-  console.log("yticks:", yticks);
+  let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`);
+  // console.log("yticks:", yticks);
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
@@ -81,13 +85,14 @@ function buildCharts(sample) {
       type: "bar",
       orientation: "h",
     }];
-    console.log("Bar Data:", barData);
+    // console.log("Bar Data:", barData);
 
     let barLayout = {
       title: "Top 10 Bacteria Cultures Found",
       margin: { t: 30, l: 150 }
     };
 
+    
     // Render the Bar Chart
     Plotly.newPlot("bar", barData, barLayout);
   });
@@ -105,14 +110,11 @@ function init() {
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
-    // option for each sample name.
-    sampleNames.forEach((sample) => {
-      selector
-          .append("option")
-          .text(sample)
-          .property("value", sample);
-  });
-  
+    // option for each sample name.   
+    
+    for(let i = 0; i<sampleNames.length; i++){
+      selector.append("option").text(sampleNames[i]).property("value",sampleNames[i]);
+    };
 
     // Get the first sample from the list
     let firstSample = sampleNames[0];
@@ -132,3 +134,4 @@ function optionChanged(newSample) {
 
 // Initialise the dashboard
 init();
+
